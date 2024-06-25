2a3b234d6591eba1ca714ced602f51f217556ff3
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-06-25 20:15:43 -0300

    adiciona try/catch

--- src/routes/servicos.routes.js
+++ src/routes/servicos.routes.js
@@ -12,18 +12,30 @@ const conexao = new Pool({
 const servicosRoutes = new Router()
 
 servicosRoutes.post('/', async (request, response) => {
-    const dados = request.body
-    
-    const servico =  await conexao.query(`
+
+    try {
+        const dados = request.body
+
+        if (!dados.nome || (!dados.preco && dados.preco !== 0)) {
+            return response.status(400).json({
+                mensagem: 'O nome e o preço são obrigatórios'
+            })
+        }
+
+        const servico = await conexao.query(`
             INSERT INTO servicos
              (nome,descricao,preco)
              values
              ($1,$2,$3)
              returning *
-        `, [dados.nome,dados.descricao,dados.preco])
+        `, [dados.nome, dados.descricao, dados.preco])
 
-    response.status(201).json(servico.rows[0])
-
+        response.status(201).json(servico.rows[0])
+    } catch (error) {
+        response.status(500).json({
+            mensagem: 'Houve um erro ao cadastrar o serviço'
+        })
+    }
 })
 
 module.exports = servicosRoutes
