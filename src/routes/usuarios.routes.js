ee9a2a2eb8190eb2bee745ba977868de800197a5
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-07-11 19:00:46 -0300

    remove codigo

--- src/routes/usuarios.routes.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express')
-const UsuarioController = require('../controllers/UsuarioController')
-
-const usuariosRoutes = new Router()
-
-usuariosRoutes.post("/", UsuarioController.criarConta)
-
-module.exports = usuariosRoutes
