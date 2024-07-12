e0c015754dce1bd6601d7713b35993583b66fa64
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-07-11 21:39:50 -0300

    adiciona rotas de login

---
+++ src/controllers/LoginController.js
@@ -0,0 +1,60 @@
+const Usuario = require("../models/Usuario")
+const { compareSync } = require('bcryptjs')
+const { sign } = require('jsonwebtoken')
+
+class LoginController {
+
+    async login(request, response) {
+        try {
+            const dados = request.body
+
+            if (!dados.email || !dados.password) {
+                return response
+                    .status(400)
+                    .json({ mensagem: 'Nome e senha são obrigatórios' })
+            }
+
+            const usuario = await Usuario.findOne({
+                where: {
+                    email: dados.email
+                }
+            })
+
+            if (!usuario) {
+                return response
+                    .status(404)
+                    .json({ mensagem: 'Conta não encontrada' })
+            }
+
+            const senhaEstaCorreta = compareSync(dados.password, usuario.password_hash)
+
+            if (senhaEstaCorreta === false) {
+                return response
+                    .status(404)
+                    .json({
+                        mensagem: 'Conta não encontrada com esse email ou senha'
+                    })
+            }
+
+            const token = sign({
+                id: usuario.id
+            },
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: '1d'
+                }
+            )
+
+            response.json({
+                token: token,
+                nome: usuario.nome
+            })
+
+        } catch (error) {
+            response.status(500).json({ mensagem: 'Erro ao realizar login' })
+        }
+    }
+
+}
+
+module.exports = new LoginController()
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,11 +3,13 @@ const { Router } = require('express')
 const responsaveisRoutes = require('./responsaveis.routes')
 const cursosRoutes = require('./cursos.routes')
 const usuariosRoutes = require('./usuarios.routes')
+const LoginController = require('../controllers/LoginController')
 
 const routes = new Router()
 
 routes.use('/responsaveis', responsaveisRoutes)
 routes.use('/cursos', cursosRoutes)
 routes.use('/usuarios', usuariosRoutes)
+routes.post('/login', LoginController.login)
 
 module.exports = routes
